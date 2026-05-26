2129c6f7701faff7b2b4b0753d309da4f21a6918
RiddhiTotala <rtotala12@gmail.com>
2026-05-26 11:17:11 +0530

    Update LoginComponent.tsx

--- todo/components/pages/LoginComponent.tsx
+++ todo/components/pages/LoginComponent.tsx
@@ -9,7 +9,7 @@ export default function LoginComponent() {
   const router = useRouter();
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:3001/auth/login", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
